92e8f8fba8fbe4165e834197a212defeee50146d
Amina Adewusi <amina.adewusi@guardian.co.uk>
2020-10-24 16:52:54 +0100

    test opening of csv file and getting correct value from it

--- twittercli/src/main/scala/example/Twitter.scala
+++ twittercli/src/main/scala/example/Twitter.scala
@@ -1,6 +1,10 @@
 package example
 
+import scala.io.Source
 import scala.util.control.NonFatal
+
+// Amina, you've completed your first test and you've written a function to parse csv!
+// You need to work on getting the csv into a Map that you need to look up the userID and return the destinationID
 
 object Twitter {
   def main(args: Array[String]): Unit = {
@@ -13,8 +17,8 @@ object Twitter {
       val tweetsWithHumanReadableTimestamp = makeTimestampHumanReadable(tweetsWithParsedTimestamp)
       val mostRecentTweets = getMostRecentTweets(tweetsWithHumanReadableTimestamp)
       val mostRecentTweetsWithDisplayName = getDisplayName(mostRecentTweets)
-      val results = results(mostRecentTweetsWithDisplayName)
-      println(results)
+      val result = results(mostRecentTweetsWithDisplayName)
+      println(result)
     } catch {
       case NonFatal(error) =>
         println(error.getMessage)
@@ -22,11 +26,13 @@ object Twitter {
   }
 
   // make sure we've got an Int from the CLI input
-  def processUserId(userId: String): Int = ???
+  def processUserId(userId: String): Int = userId.toInt
   // who does the user id follow? (follows.csv) get the destinationID
-  def getFollowers(userID: Int): Int = ???
+  def getFollowers(userID: Int): List[Long] = {
+    new FollowsRead(followsFile).read().flatMap(_.get(userID))
+  }
   // which tweets are from destinationID? destinationID, should match authorID in (tweets.csv)
-  def getTweetsFromFollowers(destinationId: Int): List[RawTweet] = ???
+  def getTweetsFromFollowers(destinationIds: List[Long]): List[RawTweet] = ???
   // parse timestamp in tweets.csv
   def parseTimestamp(tweetsWithUnparsedTimestamp: List[RawTweet]): List[Tweet] = ???
   // make timestamp human readable
@@ -37,6 +43,40 @@ object Twitter {
   def getDisplayName(tweetsWithoutDisplayName: List[Tweet]): List[Tweet] = ???
   // amalgamate results: display name, timestamp human readable, tweet text
   def results(tweets: List[Tweet]): List[String] = ???
+
+  case class RawTweet()
+
+  case class Tweet()
+
+  case class User(userId: Int, twitterScreenName: String, fullDisplayName: String)
+
+  case class Tweets(tweetId: Int, authorId: Int, timestamp: Int, text: String)
+
+  case class Follows(sourceUserId: Long, destinationUserId: Long) {
+    def transformToMap: Map[Long, Long] = Map(
+      sourceUserId -> destinationUserId
+    )
+  }
+
+  // trait responsible for reading/loading CSV file data
+  trait CsvFileReader[A] {
+    def read(): List[A]
+  }
+
+  // it would be great to split this out to make it reusable
+  class FollowsRead(val fileName: String) {
+    def read(): List[Map[Long, Long]] = {
+      val file = Source.fromFile(fileName)
+      for {
+        line <- file.getLines().toList
+        values = line.split(",")
+      } yield Map(values(0).toLong -> values(1).toLong)
+    }
+  }
+
+  val followsFile: String = "src/main/resources/data/follows.csv"
+  val usersFile: String = "src/main/resources/data/users.csv"
+  val tweetsFile: String = "src/main/resources/data/tweets.csv"
 }
 
 
--- twittercli/src/test/scala/example/TwitterSpec.scala
+++ twittercli/src/test/scala/example/TwitterSpec.scala
@@ -1,12 +1,28 @@
 package example
 
+import example.Twitter.{Follows, FollowsRead, getFollowers, processUserId}
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class TwitterSpec extends AnyFreeSpec with Matchers {
-  "accept a user id parameter" - {
-    "returns xxx for xx" in {
-      ???
+  "transform a user id parameter" - {
+    "into an Int from a String" in {
+      processUserId("1") shouldEqual 1
+    }
+    "and throw an exception if unable to transform into an Int" in {
+      an[Exception] should be thrownBy processUserId("a")
+    }
+  }
+  "get the followers of a user" - {
+    val csv = new FollowsRead("src/main/resources/data/follows.csv").read()
+    "load the followers csv file" in {
+      csv.size shouldEqual 130393
+      csv.head shouldEqual Map(989489610 -> 10224712)
+      csv(130392) shouldEqual Map(24257941 -> 4558)
+    }
+    "get the followers of a user successfully" in {
+      getFollowers(989489610).head shouldEqual 10224712
+      getFollowers(989489610).last shouldEqual 393537534
     }
   }
 }
